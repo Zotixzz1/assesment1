e3e0ceec78d734ea288a4c0aceceb574e23862bf
Zotixzz1 <161707700+Zotixzz1@users.noreply.github.com>
2026-03-12 10:42:23 +0000

    Add files via upload

---
+++ KV4012_Assessment1.py
@@ -0,0 +1,230 @@
+"""
+Use this python file as a template for your work
+Questions are listed in the PDF in the assessment area
+Make sure that it is clear which answer goes with which question
+When you are finished COPY THE WHOLE SCRIPT BELOW AND PASTE INTO THE ASSESSMENT FORM on Blackboard
+"""
+
+# COPY FROM THE LINE BELOW THIS ONE TO THE END AND PASTE INTO BLACKBOARD
+"""
+QUESTION 0
+Set the variable my_student_id to your student ID.
+[0 marks]
+"""
+
+my_student_id = ""
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 1 [1 mark]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 1
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 2 [1 mark]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 2
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 3 [1 mark]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 3
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 4 [2 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 4
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 5 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 5
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 6 [2 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 6
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 7 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 7
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 8 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 8
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 9 [4 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 9
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 10 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 10
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 11 [3 markss]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 11
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 12 [5 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 12
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 13 [6 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 13
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 14 [5 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 14
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 15 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 15
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 16 [1 mark]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 16
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 17 [2 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 17
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 18 [4 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 18
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 19 [5 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 19
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 20 [6 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 20
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 21 [2 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 21
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 22 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 22
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 23 [4 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 23
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 24 [5 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 24
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 25 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 25
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 26 [5 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 26
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 27 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 27
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 28 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 28
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 29 [3 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 29
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+QUESTION 30 [6 marks]
+"""
+
+# DELETE AND REPLACE WITH YOUR ANSWER TO QUESTION 30
+
+"""
+------------------------------------------------------------------------------------------------------------------------
+END OF QUESTIONS
+"""
